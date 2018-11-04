21343f009ed0537d0583eb925735acd3e89908e2
luis_gonzalez <luis.gonzalez@expertla.com>
2018-11-03 20:58:43 -0500

    se agrega proyecto de spring-boot, compilando, con conexion a oracle y con controladores activos

---
+++ contabilidad/src/main/java/com/facturacion/contabilidad/ContabilidadApplication.java
@@ -0,0 +1,12 @@
+package com.facturacion.contabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadApplication.class, args);
+	}
+}
